fbe977136fa6d14658ab82300c4a75e02197edaa
MisaelUrq <misaelurquieta198@hotmail.com>
2019-01-19 22:40:46 -0600

    Se probo conectar con una base de datos de prueba con MySQL. Se actualizo el driver de MySQL para Java.

--- src/com/proyecto/Main.java
+++ src/com/proyecto/Main.java
@@ -1,7 +1,23 @@
 package com.proyecto;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hola mundo!");
+        Connection conn;
+        final String nombre_base_datos = "prueba"; // NOTE(Misael): Sustituir con base de datos a usar.
+        final String usuario           = "root";   // NOTE(Misael): Sustituir con usuario.
+        final String contrasenna       = "";       // NOTE(Misael): Sustituir con contraseña.
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/"+nombre_base_datos+"?serverTimezone=CST", usuario, contrasenna);
+            System.out.println("Se pudo conectar correctamente...");
+        } catch (Exception e) {
+            System.out.println("ERROR: No sé pudo establecer una conneción con la base de datos.");
+            e.printStackTrace();
+        }
     }
 }
